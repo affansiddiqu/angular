fa0a166ceefec34536797ac084e8235f43bed59e
shahbaz hussain <sh1398496@gmail.com>
2024-11-11 11:04:28 +0500

    ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ToggleComponent } from './toggle/toggle.component';
+import { MoviesComponent } from './movies/movies.component';
+import { CreateMedicineComponent } from './create-medicine/create-medicine.component';
+import { NewComponent } from './new/new.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component:NavbarComponent
+    },
+    {
+        path:"about",
+        component:AboutComponent
+    },
+    {
+        path:"contact",
+        component:ContactComponent,
+    },
+    {
+        path:"toggle",
+        component:ToggleComponent,
+    },
+    {
+        path:"movies",
+        component:MoviesComponent,
+    },
+    {
+        path:"create-medicine",
+        component:CreateMedicineComponent,
+    },
+    {
+        path:"new",
+        component:NewComponent,
+    }
+];
---
+++ src/app/create-medicine/create-medicine.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-medicine',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-medicine.component.html',
+  styleUrl: './create-medicine.component.css'
+})
+export class CreateMedicineComponent {
+
+}
